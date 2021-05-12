c81bed861fdc4ab8aa908370c30bfe2d7c9e475b
said.ozgat <said.ozgat@hotmail.com>
2021-05-12 12:33:58 +0300

    +payment page desing bug fix 1.0.2

--- paywithiyzico/paywithiyzico.php
+++ paywithiyzico/paywithiyzico.php
@@ -43,7 +43,7 @@ class Paywithiyzico extends PaymentModule
     {
         $this->name = 'paywithiyzico';
         $this->tab = 'payments_gateways';
-        $this->version = '1.0.1';
+        $this->version = '1.0.2';
         $this->author = 'iyzico';
         $this->need_instance = 1;
 
